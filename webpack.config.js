c088dd9ebe2d2d501c186728b31b09e9b0f59ec4
Vasil Vanchuk <v.vanchuk@tut.by>
2021-02-01 20:39:11 +0200

    Add webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HandlebarsPlugin = require("handlebars-webpack-plugin");
+
 const webpack = require("webpack");
 const path = require("path");
 
@@ -23,8 +25,25 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({ template: "index.html" }),
+    
     new CleanWebpackPlugin(),
     new webpack.HotModuleReplacementPlugin(),
+    new HandlebarsPlugin({
+      // path to hbs entry file(s). Also supports nested directories if write path.join(process.cwd(), "app", "src", "**", "*.hbs"),
+      entry: path.join(process.cwd(), "pages", "*.hbs"),
+      // output path and filename(s). This should lie within the webpacks output-folder
+      // if ommited, the input filepath stripped of its extension will be used
+      output: path.join(process.cwd(), "dist", "[name].html"),
+      // you can also add a [path] variable, which will emit the files with their relative path, like
+      // output: path.join(process.cwd(), "build", [path], "[name].html"),
+
+ 
+      // globbed path to partials, where folder/filename is unique
+      partials: [
+        path.join(process.cwd(), "partials", "**", "*.hbs")
+      ],
+
+    })
   ],
   module: {
     rules: [
